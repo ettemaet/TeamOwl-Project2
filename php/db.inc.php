d97f7d5b892a9cadfeaf588c2ed3175408890689
315242904@qq.com <315242904@qq.com>
2015-04-03 11:43:02 -0400

    Change the configure

--- php/db.inc.php
+++ php/db.inc.php
@@ -3,9 +3,9 @@
 function pdo_connect() {
     try {
         // Production server
-        $dbhost="mysql:host=mysql-user.cse.msu.edu;dbname=gaojun1";
-        $user = "gaojun1";
-        $password = "A47343849";
+        $dbhost="mysql:host=mysql-user.cse.msu.edu;dbname=polavar3";
+        $user = "polavar3";
+        $password = "mobileapp476";
         return new PDO($dbhost, $user, $password);
     } catch(PDOException $e) {
         die( "Unable to select database");
