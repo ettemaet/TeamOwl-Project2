75dcd54b49b8f27be1943f30fc864a50d149a746
polavar3 <polavar3@msu.edu>
2015-04-11 18:48:26 -0400

    Ari: Added code to make "Remember Me" work

--- app/src/main/java/edu/msu/stanospa/teamowl_project2/MainActivity.java
+++ app/src/main/java/edu/msu/stanospa/teamowl_project2/MainActivity.java
@@ -2,22 +2,61 @@ package edu.msu.stanospa.teamowl_project2;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
     private Game game;
+    private final static String PREFERENCES = "preferences";
+    private final static String USERNAME = "username";
+    private final static String PASSWORD = "password";
+    private String username = "";
+    private String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         game = new Game(this);
+        readPreferences();
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        if (checked == true) {
+            username = ((EditText)findViewById(R.id.editUsername)).getText().toString();
+            password = ((EditText)findViewById(R.id.editPassword)).getText().toString();
+            writePreferences();
+        }
+    }
+
+    private void readPreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+
+        String putUsername = settings.getString(USERNAME, "");
+        String putPassword = settings.getString(PASSWORD, "");
+
+        ((EditText)findViewById(R.id.editUsername)).setText(putUsername);
+        ((EditText)findViewById(R.id.editPassword)).setText(putPassword);
+
+    }
+
+    private void writePreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+        editor.putString(USERNAME, username);
+        editor.putString(PASSWORD, password);
+
+        editor.commit();
     }
 
     public void onLogin(final View view) {
